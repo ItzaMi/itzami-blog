915b4c7480475eaa324e26db6fb6ec592475e9b5
ItzaMi <rros00@gmail.com>
2020-07-04 16:15:20 +0100

    Add introduction

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
-import React from "react"
-
+import React, { useContext } from "react"
 import styled from "styled-components"
+import { ThemeManagerContext } from "gatsby-styled-components-dark-mode"
 
 import JSONData from '../content/homepage'
 
 import SEO from "../components/seo"
 
 const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
 `
 
 const Title = styled.h1`
@@ -16,12 +20,22 @@ const Title = styled.h1`
   line-height: 90%;
 `
 
+const Introduction = styled.p`
+  font-size: 30px;
+  font-weight: bold;
+  max-width: 50%;
+`
+
 const IndexPage = () => (
   <Container>
+
     <SEO title="Home" />
-    {JSONData.title.map((data, index) => {
-      return <Title key={`content_item_${index}`}>{data.line}</Title>
-    })}
+    <div>
+      {JSONData.title.map((data, index) => {
+        return <Title key={`content_item_${index}`}>{data.line}</Title>
+      })}
+    </div>
+    <Introduction>{JSONData.introduction}</Introduction>
   </Container>
 )
 
