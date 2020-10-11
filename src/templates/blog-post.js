8899f34b1f978d22e3f8fd86a48e8f0ca28cf9ed
ItzaMi <rros00@gmail.com>
2020-10-11 12:24:12 +0100

    feature(blog-post.js) add overall styling to blog post (might need changes down the line

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -13,27 +13,60 @@ const Container = styled.div`
 
 const BlogContainer = styled.div`
   width: 60%;
+  max-width: 1000px;
   margin: auto;
+  margin-top: 3rem;
+  margin-bottom: 3rem;
+
+  h1 {
+    font-size: 3.5rem;
+  }
+`
+
+const BlogContent = styled.div`
   margin-top: 2rem;
+
+  h2,
+  h3,
+  h4 {
+    margin-top: 4rem;
+    margin-bottom: 1rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+  }
+
+  h4 {
+    font-size: 2rem;
+  }
+
+  a {
+    color: #8d9e71;
+    text-decoration: none;
+    font-weight: bold;
+  }
 `
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
-    <>
-      <Container>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.title}
-          author={post.frontmatter.title}
-        />
-        <NavBar type="small" />
-        <BlogContainer>
-          <h1>{post.frontmatter.title}</h1>
-          <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        </BlogContainer>
-      </Container>
-    </>
+    <Container>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.title}
+        author={post.frontmatter.title}
+      />
+      <NavBar type="small" />
+      <BlogContainer>
+        <h1>{post.frontmatter.title}</h1>
+        <BlogContent dangerouslySetInnerHTML={{ __html: post.html }} />
+      </BlogContainer>
+    </Container>
   )
 }
 
