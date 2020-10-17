ad0cc29aab07b181c9e6c8a947f01a002e5fa596
ItzaMi <rros00@gmail.com>
2020-10-17 10:07:59 +0100

    feature(SocialLinks.js) media query styling

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,21 +1,53 @@
-import React from "react";
-import styled from "styled-components";
+import React from "react"
+import styled from "styled-components"
 
-import Dribbble from '../icons/dribbble';
-import Twitter from '../icons/twitter';
-import Instagram from '../icons/instagram';
-import GitHub from '../icons/github';
+import Dribbble from "../icons/dribbble"
+import Twitter from "../icons/twitter"
+import Instagram from "../icons/instagram"
+import GitHub from "../icons/github"
 
 const Container = styled.div`
-
+  @media (max-width: 415px) {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+  }
 `
 
 const SocialLinks = () => (
-    <Container>
-        <a aria-label="twitter button" href="https://twitter.com/HeyItzaMi" rel="noreferrer" target="_blank"><Twitter /></a>
-        <a aria-label="github button" href="https://github.com/ItzaMi" rel="noreferrer" target="_blank"><GitHub /></a>
-        <a aria-label="instagram button" href="https://www.instagram.com/mr.tomski/" rel="noreferrer" target="_blank"><Instagram /></a>
-        <a aria-label="dribbble button" href="https://dribbble.com/ItzaMi" rel="noreferrer" target="_blank"><Dribbble /></a>
-    </Container>
+  <Container>
+    <a
+      aria-label="twitter button"
+      href="https://twitter.com/HeyItzaMi"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <Twitter />
+    </a>
+    <a
+      aria-label="github button"
+      href="https://github.com/ItzaMi"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <GitHub />
+    </a>
+    <a
+      aria-label="instagram button"
+      href="https://www.instagram.com/mr.tomski/"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <Instagram />
+    </a>
+    <a
+      aria-label="dribbble button"
+      href="https://dribbble.com/ItzaMi"
+      rel="noreferrer"
+      target="_blank"
+    >
+      <Dribbble />
+    </a>
+  </Container>
 )
-export default SocialLinks;
+export default SocialLinks
