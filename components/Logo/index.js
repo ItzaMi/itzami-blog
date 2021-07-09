150ffba46ff28edcc6e5d8cab7944abfaeb4b05b
ItzaMi <rros00@gmail.com>
2021-07-09 22:37:34 +0100

    [Logo] add link to name

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -1,14 +1,18 @@
 import React from "react"
+import Link from "next/link"
+
 import css from "./logo.module.css"
 
 const Logo = () => {
   return (
     <div className={css.host}>
-      <h1 className={css.name}>
-        Itza
-        <br />
-        Mi
-      </h1>
+      <Link href="/">
+        <a className={css.name}>
+          Itza
+          <br />
+          Mi
+        </a>
+      </Link>
     </div>
   )
 }
