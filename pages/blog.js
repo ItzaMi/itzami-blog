c732629d9def094e0f5994f8db2c32e84479ebf3
ItzaMi <rros00@gmail.com>
2021-07-11 20:01:28 +0100

    [Blog] setup to pass contentful content

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,29 @@
 import Head from "next/head"
+import { createClient } from "contentful"
 
+import PostsList from "../components/PostsList"
 import Logo from "../components/Logo"
 import Nav from "../components/Nav"
 import Info from "../components/Info"
 
-import css from "../styles/index.module.css"
+import css from "../styles/blog.module.css"
 
-const Blog = () => {
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  })
+
+  const res = await client.getEntries({ content_type: "blogPost" })
+
+  return {
+    props: {
+      blogPosts: res.items,
+    },
+  }
+}
+
+const Blog = ({ blogPosts }) => {
   return (
     <div className={css.host}>
       <Head>
@@ -20,25 +37,7 @@ const Blog = () => {
       <Logo />
       <Nav />
       <Info page="Blog" />
-      <div className={css.content}>
-        <div className={css.descriptionContainer}>
-          <p>Hey, I’m Rui 👋</p>
-          <p>
-            I’m a self-taught front-end developer with a Master’s Degree in
-            Psychology and a knack for design
-          </p>
-          <p>
-            And this is where I share my experience and knowledge with the
-            internet
-          </p>
-        </div>
-        <div className={css.postContainer}>
-          <p className={css.postCTA}>Read my latest post</p>
-          <p className={css.postTitle}>
-            How to build an accordion with JavaScript
-          </p>
-        </div>
-      </div>
+      <PostsList posts={blogPosts} className={css.content} />
     </div>
   )
 }
