eb9cd9dd64ce8da0655fbecd572d994c32f02c86
ItzaMi <rros00@gmail.com>
2021-07-11 22:06:14 +0100

    [SEO] `summary_large_image` for twitter:card

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -5,14 +5,13 @@ const SEO = ({ title, description, image }) => {
 
   return (
     <Head>
-      <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       {/* <meta property="og:url" content="https://www.dgtlnk.com/blog/https/" /> */}
       <meta property="og:site_name" content="ItzaMi" />
       <meta property="og:image" content={modifiedImageUrl} />
 
-      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:site" content="@heyitzami" />
