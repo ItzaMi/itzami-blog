a03eb0f3275d104887f6fe72b0bd4668cc2a8d70
ItzaMi <rros00@gmail.com>
2021-07-11 20:01:08 +0100

    [PostsList] Iterate over `posts` + styling

---
+++ components/PostsList/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import Link from "next/link"
+
+import css from "./posts-list.module.css"
+
+const PostsList = ({ posts = [] }) => {
+  const sortedPostsPerDate = [...posts].sort((a, b) => {
+    return new Date(b.fields.date) - new Date(a.fields.date)
+  })
+
+  return (
+    <div className={css.host}>
+      {sortedPostsPerDate.map(post => (
+        <Link key={post.sys.id} href={`/blog/${post.fields.slug}`}>
+          <a className={css.postContainer}>
+            <p className={css.postTitle}>{post.fields.title}</p>
+            <p className={css.postDescription}>{post.fields.description}</p>
+          </a>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default PostsList
