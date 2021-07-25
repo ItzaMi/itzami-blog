d938d77d3487ba771981c496571cc9386d21fa8b
ItzaMi <rros00@gmail.com>
2021-07-25 11:54:07 +0100

    [CodeBlock] change `dracula` to `nord`

--- components/CodeBlock/index.js
+++ components/CodeBlock/index.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { nord } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
 const CodeBlock = {
   code({ node, inline, className, children, ...props }) {
     const match = /language-(\w+)/.exec(className || "")
     return !inline && match ? (
       <SyntaxHighlighter
-        style={dracula}
+        style={nord}
         language={match[1]}
         PreTag="div"
         {...props}
