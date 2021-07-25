5374ba5aae750c44605f2093c56dc0a1aaba0676
ItzaMi <rros00@gmail.com>
2021-07-25 18:28:05 +0100

    [Nav] style links when active

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,15 +1,30 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 import css from "./nav.module.css"
 
 const Nav = () => {
+  const router = useRouter()
+
   return (
     <div className={css.host}>
       <Link href="/blog">
-        <a className={css.link}>Blog</a>
+        <a
+          className={`${css.link} ${
+            router.pathname.includes("blog") ? css.active : ""
+          }`}
+        >
+          Blog
+        </a>
       </Link>
       <Link href="/about">
-        <a className={css.link}>About</a>
+        <a
+          className={`${css.link} ${
+            router.pathname === "/about" ? css.active : ""
+          }`}
+        >
+          About
+        </a>
       </Link>
     </div>
   )
