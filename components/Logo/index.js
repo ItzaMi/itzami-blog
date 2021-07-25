2ee2097748d6b4fc5b80634d2010ccfcad16b025
ItzaMi <rros00@gmail.com>
2021-07-25 22:43:11 +0100

    [Logo] Pass `className` prop

--- components/Logo/index.js
+++ components/Logo/index.js
@@ -3,9 +3,9 @@ import Link from "next/link"
 
 import css from "./logo.module.css"
 
-const Logo = () => {
+const Logo = ({ className }) => {
   return (
-    <div className={css.host}>
+    <div className={`${css.host} ${className}`}>
       <Link href="/">
         <a className={css.name}>
           Itza
