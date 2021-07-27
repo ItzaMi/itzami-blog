21762aa7df12fd8de5a8a28b5a225ea6465c62f3
ItzaMi <rros00@gmail.com>
2021-07-27 21:43:51 +0100

    [SEO] pass just `image` prop

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -1,15 +1,13 @@
 import Head from "next/head"
 
 const SEO = ({ title, description, image }) => {
-  const modifiedImageUrl = "https:" + image
-
   return (
     <Head>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       {/* <meta property="og:url" content="https://www.dgtlnk.com/blog/https/" /> */}
       <meta property="og:site_name" content="ItzaMi" />
-      <meta property="og:image" content={modifiedImageUrl} />
+      <meta property="og:image" content={image} />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
