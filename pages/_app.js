4aef7ee0f32848e8996828e385a31d100ab4bbdd
ItzaMi <rros00@gmail.com>
2021-08-02 21:23:22 +0100

    [Setup] Add Google Analytics setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,24 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
+  // https://flaviocopes.com/nextjs-google-analytics/
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      window.gtag("config", process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      })
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
 
